9e000395b866a0bdc61d4028d10532ed8bddbeb4
Jonah Wilson <jdbw13@hotmail.com>
2024-11-21 09:49:17 -0700

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import transactionsSlice, { deposit, withdrawal, transfer } from "../features/transactions/transactionsSlice";
+
 
 // TODO: Configure the store to use the reducer from the transactions slice.
-const store = configureStore();
-
-export default store;
+const store = configureStore({
+    reducer: {
+       
+        transactions: transactionsSlice,
+        deposit: deposit,
+        withdrawal: withdrawal,
+        transfer: transfer, 
+    },
+});
+export default store
